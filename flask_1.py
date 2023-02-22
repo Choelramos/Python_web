2731d260df0d07694f4c9e74995d9c442178cdad
Joel <joel4785ramos@gmail.com>
2023-02-21 22:31:36 -0300

    Adicionando uma página

--- flask_1.py
+++ flask_1.py
@@ -3,7 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/pagina')
+def pagina():
+    return "Página inicial!"
+
+
+@app.route('/ola')
 def oi():
     return "Hello World!"
 
